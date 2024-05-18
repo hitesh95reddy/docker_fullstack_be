6cd499720975f6d15f7cdc8a77cf9a56f6afafe2
hitesh95reddy <33710615+hitesh95reddy@users.noreply.github.com>
2024-05-18 15:31:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from database import engine
+from models import Base
+
+from auth.routers import router as authRouter
+app=FastAPI()
+app.include_router(authRouter,prefix='/auth')
+
+# Base.metadata.create_all(bind=engine)
+
+@app.get('/')
+def home():
+    return {"msg":"welcome to my home page!!"}
