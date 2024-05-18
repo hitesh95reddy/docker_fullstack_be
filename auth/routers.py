627cc77082877105713c2e927292be1687e96911
hitesh95reddy <33710615+hitesh95reddy@users.noreply.github.com>
2024-05-18 19:29:16 +0530

    Update routers.py

--- auth/routers.py
+++ auth/routers.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 from models import Users
 from database import SessionLocal
 from sqlalchemy.orm import session
 from .schemas import RegisterReq,LoginReq
 router=APIRouter()
+
+oauth_schema=OAuth2PasswordBearer(tokenUrl='/auth/token')
 
 def get_db():
     try:
@@ -11,6 +14,23 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@router.post('/token')
+def getToken(form_data:OAuth2PasswordRequestForm=Depends(),db:session=Depends(get_db)):
+    user=db.query(Users).filter(Users.uname==form_data.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Invalid User')
+    elif not (user.password==form_data.password):
+        raise HTTPException(status_code=404,detail='Invalid Credentials')
+    else:
+        return {'access_token':form_data.username+"::"+user.email}
+
+
+@router.get('/current_user')
+def get_current_user(token:str=Depends(oauth_schema)):
+    print(token)
+    return {'user':token.split("::")[0],'email':token.split("::")[1]}
+
 
 @router.get("/users")
 def allUsers(db:session=Depends(get_db)):
